8b9f505d28facb3c2217721021fc99eaadbfb8b5
beonseong7 <beonseong7@naver.com>
2024-05-25 23:49:37 +0900

    mobanimation

--- Assets/Scripts/GameScript/MobScript.cs
+++ Assets/Scripts/GameScript/MobScript.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class MobScript : MonoBehaviour
 {
     int corner = 0;
+    Animator animator;
     [Header("Status")]
     public float speed;
     public float Hp;
     public float physicArmor;
     public float magicArmor;
+    public int turn_speed;
     void Start()
     {
+        animator=this.GetComponent<Animator>();
         StartCoroutine(MobMove());
     }
 
@@ -36,32 +39,54 @@ public class MobScript : MonoBehaviour
                 break;
         }
         yield return new WaitForSeconds(0.01f);
-        IsArrive();
+        StartCoroutine(IsArrive());
     }
 
-    void IsArrive()
+    IEnumerator IsArrive()
     {
         if (transform.position == new Vector3(-1607, 6, -1612))
         {
+
+            for(int i = 0;i<90/ Mathf.Abs(turn_speed); i++)
+            {
+                transform.Rotate(new Vector3(0, turn_speed, 0));
+                yield return new WaitForSeconds(0.01f);
+            }
             transform.rotation = Quaternion.Euler(0, 90, 0);
             corner = 1;
         }
         else if (transform.position == new Vector3(1597, 6, -1612))
         {
+            for (int i = 0; i < 90 / Mathf.Abs(turn_speed); i++)
+            {
+                transform.Rotate(new Vector3(0, turn_speed, 0));
+                yield return new WaitForSeconds(0.01f);
+            }
             transform.rotation = Quaternion.Euler(0, 0, 0);
             corner = 2;
         }
         else if (transform.position == new Vector3(1597, 6, 1421))
         {
+            for (int i = 0; i < 90 / Mathf.Abs(turn_speed); i++)
+            {
+                transform.Rotate(new Vector3(0, turn_speed, 0));
+                yield return new WaitForSeconds(0.01f);
+            }
             transform.rotation = Quaternion.Euler(0, 270, 0);
             corner = 3;
         }
         else if (transform.position == new Vector3(-1607, 6, 1421))
         {
+            for (int i = 0; i < 90 / Mathf.Abs(turn_speed); i++)
+            {
+                transform.Rotate(new Vector3(0, turn_speed * Time.deltaTime, 0));
+                yield return new WaitForSeconds(0.01f);
+            }
             transform.rotation = Quaternion.Euler(0, 180, 0);
             corner = 0;
     }
         StartCoroutine(MobMove());
     }
+    OnAnimation
 
 }
