e51389ce844aa8ca0913c643acac49613bc16b3a
beonseong7 <beonseong7@naver.com>
2024-06-02 23:23:57 +0900

    UILayOut

--- Assets/Scripts/GameScript/InGameManager.cs
+++ Assets/Scripts/GameScript/InGameManager.cs
@@ -7,7 +7,7 @@ public class InGameManager : MonoBehaviour
 {
     public Button Button;
     public GameObject[] Panels;
-    public string[,] Charac_Name =new string [,] { { "블루노","후쿠로","니코 로빈","베포","브룩","이나즈마","우솝","타시기","쵸파","페로나","에이스","프랑키","하찌"},{"루피"},{ "루피"},{"루피"} };
+    public Dictionary<string, string> Charac_Name;
     void Start()
     {
         foreach(var tmp in Panels)
