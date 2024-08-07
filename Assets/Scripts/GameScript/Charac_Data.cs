17441b4d3932eef6a4d238a8b7aa4b7ec8f246b6
beonseong7 <beonseong7@naver.com>
2024-08-07 23:31:58 +0900

    RandomDefence Recipe

--- Assets/Scripts/GameScript/Charac_Data.cs
+++ Assets/Scripts/GameScript/Charac_Data.cs
@@ -8,9 +8,13 @@ public class Charac_Data
      Dictionary<string,CharacData> Charac_status= new Dictionary<string, CharacData>();
     public void Set_Status()
     {
-        Charac_status["커먼"]= new CharacData("루피", 0.0f, basicType.Melee, 5.0f, 4.0f,new SkillData[] { new SkillData("Arrow", skillType.Physical, 0.0f, 10.0f) });
-        Charac_status["언커먼"] = new CharacData("루피", 0.0f, basicType.Ranged, 10.0f, 4.0f, new SkillData[] { new SkillData("Arrow", skillType.Physical, 0.0f, 10.0f) });
-        Charac_status["레어"] = new CharacData("루피", 0.0f, basicType.Melee, 20.0f, 4.0f, new SkillData[] { new SkillData("Arrow", skillType.Physical, 0.0f, 10.0f) });
+        Charac_status["Braker"]= new CharacData("Braker", 0.0f, basicType.Melee, 5.0f, 4.0f,new SkillData[] { new SkillData("Slash", skillType.Physical, 0.0f, 10.0f) });
+        Charac_status["Striker"] = new CharacData("Striker", 0.0f, basicType.Melee, 5.0f, 4.0f, new SkillData[] { new SkillData("Slash", skillType.Physical, 0.0f, 10.0f) });
+        Charac_status["Devilhunter"] = new CharacData("Devilhunter", 0.0f, basicType.Melee, 5.0f, 4.0f, new SkillData[] { new SkillData("Slash", skillType.Physical, 0.0f, 10.0f) });
+        Charac_status["Meteorogist"] = new CharacData("Meteorogist", 0.0f, basicType.Melee, 5.0f, 4.0f, new SkillData[] { new SkillData("Slash", skillType.Physical, 0.0f, 10.0f) });
+        Charac_status["Gunslinger"] = new CharacData("Gunslinger", 0.0f, basicType.Melee, 5.0f, 4.0f, new SkillData[] { new SkillData("Slash", skillType.Physical, 0.0f, 10.0f) });
+        Charac_status["Warload"] = new CharacData("Warload", 0.0f, basicType.Ranged, 10.0f, 4.0f, new SkillData[] { new SkillData("Arrow", skillType.Physical, 0.0f, 10.0f) });
+        Charac_status["Holyknight"] = new CharacData("Holyknight", 0.0f, basicType.Melee, 20.0f, 4.0f, new SkillData[] { new SkillData("Arrow", skillType.Physical, 0.0f, 10.0f) });
         Charac_status["에픽"] = new CharacData("루피", 0.0f, basicType.Ranged, 40.0f, 4.0f, new SkillData[] { new SkillData("Arrow", skillType.Physical, 0.0f, 10.0f) });
         Charac_status["레전더리"] = new CharacData("루피", 0.0f, basicType.Ranged, 80.0f, 4.0f, new SkillData[] { new SkillData("Arrow", skillType.Physical, 0.0f, 10.0f) });
     }
--- README.md
+++ README.md
@@ -57,3 +57,4 @@
 2024.8.04 RandomDefence 조합법 조정  
 2024.8.05 MUFI 보고서  
 2024.8.06 RandomDefence 조합법 조정  
+2024.8.07 RandomDefence 조합법 조정  
